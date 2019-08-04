9ade53d08733ab0b487601cf6f944c9643a55091
Chris Wilson <christopher.e.wilson@gmail.com>
2019-08-04 11:24:38 -0400

    v0.6.5

--- prototype/webpack/webpack.config.js
+++ prototype/webpack/webpack.config.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const webpack = require('webpack');
-const postcssPresetEnv = ;
+const postcssPresetEnv = require('postcss-preset-env');
 const cssnano = require('cssnano');
 
 module.exports = {
